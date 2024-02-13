73ac598afc729e91ef0a839175f67644d8910689
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2024-02-13 15:56:55 -0400

    Clase uno, sintaxis basica en Java

---
+++ src/main/java/ciaf/prograIII/clasesintaxis01/EjemploCiclosYFigurasHard.java
@@ -0,0 +1,39 @@
+package ciaf.prograIII.clasesintaxis01;
+
+
+
+public class EjemploCiclosYFigurasHard {
+
+    public static void main(String[] args) {
+        // Dibujar un triángulo con asteriscos
+        System.out.println("Triángulo:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Dibujar un rombo con asteriscos
+        System.out.println("\nRombo:");
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
