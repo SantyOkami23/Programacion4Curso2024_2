caac24e18b247be95b37ff720b8a84d9e535045a
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2024-02-27 13:39:50 -0500

    Estructuras de Datos

--- src/main/java/ciaf/prograIII/claseciclosycondicionales02/TablaDeMultiPlicar7.java
+++ src/main/java/ciaf/prograIII/claseciclosycondicionales02/TablaDeMultiPlicar7.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TablaDeMultiPlicar7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese un número entero: ");
+        System.out.print("Ingrese un numero entero: ");
         int numero = scanner.nextInt();
 
         System.out.println("Tabla de multiplicar del " + numero + ":");
